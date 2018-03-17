2b68b4cf606f1ed2ba06ada3dc997da697bf1731
Bogdan01234 <32296564+Bogdan01234@users.noreply.github.com>
2018-03-17 17:50:26 +0300

    release v2

--- MoodSensor/src/Controller.java
+++ MoodSensor/src/Controller.java
@@ -1,3 +1,6 @@
+// Программа для определения настроения польователя, версия 1.0
+//Лапатнёв Богдан Александрович, группа pois1709v2.
+
 public class Controller {
 
     public static void main(String[] args) {
